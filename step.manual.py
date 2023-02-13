e9fcb0c076fa72570242ed3c7f4e8fa031799625
buslovaev_ma <buslovaev_ma@outlook.com>
2023-02-13 08:47:18 +0000

    step manual

--- step.manual.py
+++ step.manual.py
@@ -1,5 +1,6 @@
 from sinara.step import Step
 from sinara.step import StepSafeguard as sg
+from sinara.step import StepReport as sr
 
 sg.git_reset()
 
@@ -11,4 +12,5 @@ try:
 except Exception as e:
     step.handle_exception(e)
 finally:
+    sr.tag_commit_by_run()
     step.handle_exit()
